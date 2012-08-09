5d45382cf9cea6f075a1a6294755eaa876fc7426
Lou Sacco <occasl@gmail.com>
2012-08-09 12:41:33 -0700

    Cleaned up file

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+#!/bin/env node
+
 var express = require('express');
 var mongoose = require('mongoose');
-var app = module.exports = express.createServer();
+var app = express();
 
-mongoose.connect('mongodb://localhost/dogtag');
+//  Get the environment variables we need.
+var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP;
+var port    = process.env.OPENSHIFT_INTERNAL_PORT || 80;
+var dbhost  = process.env.OPENSHIFT_NOSQL_DB_HOST;
+var dbport  = process.env.OPENSHIFT_NOSQL_DB_PORT;
+var dbuname = process.env.OPENSHIFT_NOSQL_DB_USERNAME;
+var dbpwd   = process.env.OPENSHIFT_NOSQL_DB_PASSWORD;
+
+// Establish connection to MongoDB
+mongoose.connect('mongodb://'+dbuname+':'+dbpwd+'@'+dbhost+':'+dbport+'/nodetest');
 app.configure(function () {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
@@ -12,9 +23,14 @@ app.configure(function () {
 // set up the RESTful API, handler methods are defined in api.js
 var api = require('./controller/api.js');
 app.post('/dogtag', api.post);
+app.get('/dogtag/:lon/:lat/:dist?', api.near);
 app.get('/dogtag/:name/:descr/:latitude/:longitude?', api.save);
 app.get('/dogtag/:name.:format?', api.show);
 app.get('/dogtag', api.list);
 
-app.listen(8080);
-console.log("DogTag server listening on port %d", app.address().port);
+
+//  And start the app on that interface (and port).
+app.listen(port, ipaddr, function() {
+   console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
+               ipaddr, port);
+});
