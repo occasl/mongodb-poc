2b7e80c671032c6a208a5df4581afb7c63aaaaa8
Lou Sacco <lsacco@qualcomm.com>
2012-08-16 11:50:36 -0700

    Update docs and add copyright.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 mongodb-poc
 ===========
 
-Example with MongoDB, Mongoose and Node.js.
+Example geospatial app with MongoDB, Mongoose and Node.js.
+
+See my blog post here for details on how to use:
+
+http://loutilities.wordpress.com/2012/08/14/mobile-geolocation-app-in-30-minutes-part-2-sencha-touch/
+
+Note that you can import the dogs.json into OpenShifts MongoDB using the following:
+
+mongoimport --host $OPENSHIFT_NOSQL_DB_HOST --port $OPENSHIFT_NOSQL_DB_PORT --username $OPENSHIFT_NOSQL_DB_USERNAME --password $OPENSHIFT_NOSQL_DB_PASSWORD --db [DB_NAME] --collection dogtags --file dogs.json --jsonArray
+
+But after the import you'll want to create an coords field and index it for geospatial queries as follows:
+
+db.dogtags.find().forEach( function (e) { e.coords = [ e.longitude, e.latitude ]; db.dogtags.save(e); });
+db.dogtags.ensureIndex( { coords : "2d" } )
--- controller/api.js
+++ controller/api.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2012., Qualcomm, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 var DogTag = require('../model/dogtag.js');
 
 exports.post = function(req, res) {
@@ -29,13 +46,11 @@ exports.list = function(req, res) {
     });
 }
 
-// first locates a dog by its name
 exports.show = (function(req, res) {
     DogTag.findOne({name: req.params.name}, function(error, dogtag) {
         res.send([{Dog: dogtag}]);
     })
 });
-
 
 exports.near = function(req, res) {
     DogTag.find({coords : { $near : [req.params.lon, req.params.lat], $maxDistance : req.params.dist/68.91}}, function (error, dogtag) {        
